931f3422d8de106c29bba1b708d6a5814ccaf6f7
Patrick Vane <patrick_vane@hotmail.com>
2013-03-02 17:26:03 +0100

    Keyword bug fixes

--- src/com/patrick_vane/unrealscript/editor/console/UDKLaunchLogConsolePrintStream.java
+++ src/com/patrick_vane/unrealscript/editor/console/UDKLaunchLogConsolePrintStream.java
@@ -9,10 +9,14 @@ public class UDKLaunchLogConsolePrintStream extends PrintStream
 {
 	private static final String		LAUNCH_LOG_TAG					= "ScriptLog: ";
 	private static final int		LAUNCH_LOG_TAG_LENGTH			= LAUNCH_LOG_TAG.length();
-	private static final String		LAUNCH_WARNING_TAG				= "ScriptWarning: ";
-	private static final int		LAUNCH_WARNING_TAG_LENGTH		= LAUNCH_WARNING_TAG.length();
-	private static final String		LAUNCH_ERROR_TAG				= "Error: ";
-	private static final int		LAUNCH_ERROR_TAG_LENGTH			= LAUNCH_ERROR_TAG.length();
+	private static final String		LAUNCH_WARNING1_TAG				= "Warning: ";
+	private static final int		LAUNCH_WARNING1_TAG_LENGTH		= LAUNCH_WARNING1_TAG.length();
+	private static final String		LAUNCH_WARNING2_TAG				= "ScriptWarning: ";
+	private static final int		LAUNCH_WARNING2_TAG_LENGTH		= LAUNCH_WARNING2_TAG.length();
+	private static final String		LAUNCH_ERROR1_TAG				= "Error: ";
+	private static final int		LAUNCH_ERROR1_TAG_LENGTH		= LAUNCH_ERROR1_TAG.length();
+	private static final String		LAUNCH_ERROR2_TAG				= "ScriptError: ";
+	private static final int		LAUNCH_ERROR2_TAG_LENGTH		= LAUNCH_ERROR2_TAG.length();
 	
 	private int						logMode							= 0;
 	
@@ -44,8 +48,10 @@ public class UDKLaunchLogConsolePrintStream extends PrintStream
 			}
 			
 			int logCharAt 		= line.indexOf( LAUNCH_LOG_TAG );
-			int warningCharAt 	= line.indexOf( LAUNCH_WARNING_TAG );
-			int errorCharAt 	= line.indexOf( LAUNCH_ERROR_TAG );
+			int warning1CharAt 	= line.indexOf( LAUNCH_WARNING1_TAG );
+			int warning2CharAt 	= line.indexOf( LAUNCH_WARNING2_TAG );
+			int error1CharAt 	= line.indexOf( LAUNCH_ERROR1_TAG );
+			int error2CharAt 	= line.indexOf( LAUNCH_ERROR2_TAG );
 			
 			String prefix = "";
 			if( logCharAt >= 0 )
@@ -55,19 +61,33 @@ public class UDKLaunchLogConsolePrintStream extends PrintStream
 					prefix = line.substring( 0, logCharAt ) + " ";
 				UDKLaunchLogConsole.out.println( prefix + "[INFO] " + line.substring(logCharAt+LAUNCH_LOG_TAG_LENGTH) );
 			}
-			else if( warningCharAt >= 0 )
+			else if( warning1CharAt >= 0 )
 			{
 				logMode = 2;
-				if( warningCharAt > 0 )
-					prefix = line.substring( 0, warningCharAt ) + " ";
-				UDKLaunchLogConsole.warn.println( prefix + "[WARNING] " + line.substring(warningCharAt+LAUNCH_WARNING_TAG_LENGTH) );
+				if( warning1CharAt > 0 )
+					prefix = line.substring( 0, warning1CharAt ) + " ";
+				UDKLaunchLogConsole.warn.println( prefix + "[WARNING] " + line.substring(warning1CharAt+LAUNCH_WARNING1_TAG_LENGTH) );
 			}
-			else if( errorCharAt >= 0 )
+			else if( warning1CharAt >= 0 )
+			{
+				logMode = 2;
+				if( warning2CharAt > 0 )
+					prefix = line.substring( 0, warning2CharAt ) + " ";
+				UDKLaunchLogConsole.warn.println( prefix + "[WARNING] " + line.substring(warning2CharAt+LAUNCH_WARNING2_TAG_LENGTH) );
+			}
+			else if( error1CharAt >= 0 )
 			{
 				logMode = 3;
-				if( errorCharAt > 0 )
-					prefix = line.substring( 0, errorCharAt ) + " ";
-				UDKLaunchLogConsole.err.println( prefix + "[ERROR] " + line.substring(errorCharAt+LAUNCH_ERROR_TAG_LENGTH) );
+				if( error1CharAt > 0 )
+					prefix = line.substring( 0, error1CharAt ) + " ";
+				UDKLaunchLogConsole.err.println( prefix + "[ERROR] " + line.substring(error1CharAt+LAUNCH_ERROR1_TAG_LENGTH) );
+			}
+			else if( error2CharAt >= 0 )
+			{
+				logMode = 3;
+				if( error2CharAt > 0 )
+					prefix = line.substring( 0, error2CharAt ) + " ";
+				UDKLaunchLogConsole.err.println( prefix + "[ERROR] " + line.substring(error2CharAt+LAUNCH_ERROR2_TAG_LENGTH) );
 			}
 			else
 			{
